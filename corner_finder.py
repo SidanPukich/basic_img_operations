d6d3288b868d536d7d67c9f5c8c3635b72f4d066
Nikita Nesterov <nik.nest1@gmail.com>
2021-07-28 03:48:00 +0200

    added: visualization of corners and rois

--- corner_finder.py
+++ corner_finder.py
@@ -1,9 +1,8 @@
+import argparse
 import cv2
 import time
 import numpy as np
 import math
-import matplotlib.pyplot as plt
-from concurrent import futures
 from multiprocessing import Pool
 import os
 
@@ -23,7 +22,6 @@ def find_ul_offset(ul):
 
     return (o_x, o_y) 
         
-
 def find_ur_offset(ur):
     o_y = -1
     o_x = -1
@@ -72,8 +70,6 @@ def find_lr_offset(lr):
 
     return (o_x, o_y)
 
-
-
 def count_sum(sub_img, corner, pos):
     corner_w = corner.shape[1]
     corner_h = corner.shape[0]
@@ -102,7 +98,7 @@ def img_iter(img, corner, roi):
         for iw in range(roi_offset_x, roi_offset_x + roi_width - corner_w + 1):
             # print(f"{ih, iw, corner_h, corner_w, img_h - corner_h + 1 - roi_height, img_w - corner_w + 1 - roi_width}")
             yield img[ih:ih + corner_h, iw:iw + corner_w], corner, (iw, ih)
-    print("img iter prepared")            
+    # print("img iter prepared")            
 
 def find_corner(img, corner, roi):
     smallest_sum = math.inf
@@ -111,7 +107,7 @@ def find_corner(img, corner, roi):
     with Pool() as pool:
         sums = list(pool.starmap(count_sum, img_iter(img, corner, roi)))
 
-    print(f"sums counted {len(sums)}")
+    # print(f"sums counted {len(sums)}")
 
     for csum, pos in sums:
         if csum < smallest_sum:
@@ -134,81 +130,137 @@ def print_roi(img, roi, path):
 
     cv2.imwrite(path, roi_img)
 
-path_evaluation = "data/boegen/Bogen1.jpg"
-path_ul = "data/masks/ul.png"
-path_ur = "data/masks/ur.png"
-path_ll = "data/masks/ll.png"
-path_lr = "data/masks/lr.png"
+def find_corners(boegen_path, masks_path, rois):
+    if not os.path.exists(boegen_path):
+        raise Exception(f"Boegen folder {boegen_path} doesn't exist")
 
-evaluation = cv2.imread(path_evaluation, cv2.IMREAD_GRAYSCALE)
-ul = cv2.imread(path_ul, cv2.IMREAD_GRAYSCALE)
-ur = cv2.imread(path_ur, cv2.IMREAD_GRAYSCALE)
-ll = cv2.imread(path_ll, cv2.IMREAD_GRAYSCALE)
-lr = cv2.imread(path_lr, cv2.IMREAD_GRAYSCALE)
+    boegen = []
 
-if evaluation is not None and ul is not None and ur is not None and ll is not None and lr is not None:
-    print("images Loaded")
-else:
-    print("images not found")
+    for f in os.listdir(boegen_path):
+        if os.path.isdir(f):
+            continue
+        boegen += [os.path.join(boegen_path, f)]
 
+    masks = [
+        os.path.join(masks_path, "ul.png"),
+        os.path.join(masks_path, "ur.png"),
+        os.path.join(masks_path, "ll.png"),
+        os.path.join(masks_path, "lr.png")
+        ]
 
-ul_o = find_ul_offset(ul)
-ur_o = find_ur_offset(ur)
-ll_o = find_ll_offset(ll)
-lr_o = find_lr_offset(lr)
+    for mask in masks:
+        if not os.path.exists(mask):
+            raise Exception(f"File {mask} doesn't exist")
 
-# lux, luy, w, h 
-ul_roi = (0.3, 0.1, 0.2, 0.2)
-ur_roi = (0.75, 0.1, 0.25, 0.2)
-ll_roi = (0, 0.85, 0.2, 0.15)
-lr_roi = (0.85, 0.8, 0.15, 0.2)
+    ul = cv2.imread(masks[0], cv2.IMREAD_GRAYSCALE)
+    ur = cv2.imread(masks[1], cv2.IMREAD_GRAYSCALE)
+    ll = cv2.imread(masks[2], cv2.IMREAD_GRAYSCALE)
+    lr = cv2.imread(masks[3], cv2.IMREAD_GRAYSCALE)
 
-print_roi(evaluation, ul_roi, "ul_roi.jpg")
-print_roi(evaluation, ur_roi, "ur_roi.jpg")
-print_roi(evaluation, ll_roi, "ll_roi.jpg")
-print_roi(evaluation, lr_roi, "lr_roi.jpg")
+    ul_o = find_ul_offset(ul)
+    ur_o = find_ur_offset(ur)
+    ll_o = find_ll_offset(ll)
+    lr_o = find_lr_offset(lr)
 
+    corners = {}
 
-print(f"detected offset for ul: {ul_o}")
-print(f"detected offset for ur: {ur_o}")
-print(f"detected offset for ll: {ll_o}")
-print(f"detected offset for lr: {lr_o}")
+    test_dir = os.path.join(os.path.dirname(boegen_path), "test")
+    
+    if os.path.exists(test_dir):
+        for f in os.listdir(test_dir):
+            os.remove(os.path.join(test_dir, f))
+        os.rmdir(test_dir)
+    os.mkdir(test_dir)
 
-start = time.perf_counter()
-ul_pos, csum_ul = find_corner(evaluation, ul, ul_roi)
-endul = time.perf_counter()
-ur_pos, csum_ur = find_corner(evaluation, ur, ur_roi)
-endur = time.perf_counter()
-ll_pos, csum_ll = find_corner(evaluation, ll, ll_roi)
-endll = time.perf_counter()
-lr_pos, csum_lr = find_corner(evaluation, lr, lr_roi)
-endlr = time.perf_counter()
+    i = 0
+    for path_evaluation in boegen:
+        evaluation = cv2.imread(path_evaluation, cv2.IMREAD_GRAYSCALE)
 
+        # start = time.perf_counter()
+        ul_pos, csum_ul = find_corner(evaluation, ul, rois[0])
+        # endul = time.perf_counter()
+        ur_pos, csum_ur = find_corner(evaluation, ur, rois[1])
+        # endur = time.perf_counter()
+        ll_pos, csum_ll = find_corner(evaluation, ll, rois[2])
+        # endll = time.perf_counter()
+        lr_pos, csum_lr = find_corner(evaluation, lr, rois[3])
+        # endlr = time.perf_counter()
 
-print(f"detected pos for ul: {ul_pos} in: {-start + endul} sec")
-print(f"detected pos for ur: {ur_pos} in: {-endul + endur} sec")
-print(f"detected pos for ll: {ll_pos} in: {-endur + endll} sec")
-print(f"detected pos for lr: {lr_pos} in: {-endll + endlr} sec")
-print(f"total time: {-start + endlr} sec")
+        # print(f"detected pos for ul: {ul_pos} in: {-start + endul} sec")
+        # print(f"detected pos for ur: {ur_pos} in: {-endul + endur} sec")
+        # print(f"detected pos for ll: {ll_pos} in: {-endur + endll} sec")
+        # print(f"detected pos for lr: {lr_pos} in: {-endll + endlr} sec")
+        # print(f"total time: {-start + endlr} sec")
 
-evaluation = cv2.cvtColor(evaluation, cv2.COLOR_GRAY2RGB)
-ul = cv2.cvtColor(ul, cv2.COLOR_GRAY2RGB)
-ur = cv2.cvtColor(ur, cv2.COLOR_GRAY2RGB)
-ll = cv2.cvtColor(ll, cv2.COLOR_GRAY2RGB)
-lr = cv2.cvtColor(lr, cv2.COLOR_GRAY2RGB)
+        evaluation = cv2.cvtColor(evaluation, cv2.COLOR_GRAY2RGB)
 
-evaluation_marked = evaluation
+        ul_pos = (ul_pos[0] + ul_o[0], ul_pos[1] + ul_o[1])
+        ur_pos = (ur_pos[0] + ur_o[0], ur_pos[1] + ur_o[1])
+        ll_pos = (ll_pos[0] + ll_o[0], ll_pos[1] + ll_o[1])
+        lr_pos = (lr_pos[0] + lr_o[0], lr_pos[1] + lr_o[1])
 
-ul_pos = (ul_pos[0] + ul_o[0], ul_pos[1] + ul_o[1])
-ur_pos = (ur_pos[0] + ur_o[0], ur_pos[1] + ur_o[1])
-ll_pos = (ll_pos[0] + ll_o[0], ll_pos[1] + ll_o[1])
-lr_pos = (lr_pos[0] + lr_o[0], lr_pos[1] + lr_o[1])
+        evaluation_marked = cv2.circle(evaluation, ul_pos, radius=15, color=(0, 0, 255), thickness=3)
+        evaluation_marked = cv2.circle(evaluation, ur_pos, radius=15, color=(0, 255, 0), thickness=3)
+        evaluation_marked = cv2.circle(evaluation, ll_pos, radius=15, color=(255, 0, 0), thickness=3)
+        evaluation_marked = cv2.circle(evaluation, lr_pos, radius=15, color=(0, 0, 0), thickness=3)
 
-evaluation_marked = cv2.circle(evaluation_marked, ul_pos, radius=15, color=(0, 0, 255), thickness=3)
-evaluation_marked = cv2.circle(evaluation_marked, ur_pos, radius=15, color=(0, 255, 0), thickness=3)
-evaluation_marked = cv2.circle(evaluation_marked, ll_pos, radius=15, color=(255, 0, 0), thickness=3)
-evaluation_marked = cv2.circle(evaluation_marked, lr_pos, radius=15, color=(0, 0, 0), thickness=3)
+        cv2.imwrite(os.path.join(test_dir, path_evaluation), evaluation_marked)
+        corners[path_evaluation] = (ul_pos, ur_pos, ll_pos, lr_pos)
+        i += 1
+        print(f"[{i} / {len(boegen)}]: Corners detected on {path_evaluation}. Corners are {(ul_pos, ur_pos, ll_pos, lr_pos)}")
+    return corners
 
-cv2.imwrite("test.jpg", evaluation_marked)
+def draw_corners_n_rois(corners, vis_path, rois):
+    for i in corners.items():
+        f = i[0]
+        cs = i[1]
+
+        evaluation = cv2.imread(f)
+
+        for corner in cs:
+            evaluation = cv2.circle(evaluation, corner, radius=15, color=(0, 0, 255), thickness=3)
+
+        img_w = evaluation.shape[1]
+        img_h = evaluation.shape[0]
+        
+        for roi in rois:
+            roi_offset_x = (int)(roi[0] * img_w)
+            roi_offset_y = (int)(roi[1] * img_h)
+            roi_width = (int)(roi[2] * img_w)   
+            roi_height = (int)(roi[3] * img_h)  
+
+            evaluation = cv2.rectangle(evaluation, (roi_offset_x, roi_offset_y), (roi_offset_x + roi_width, roi_offset_y + roi_height), color=(255, 0, 0), thickness=3)
+
+        cv2.imwrite(os.path.join(vis_path, os.path.basename(f)), evaluation)
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--boegen_path", default="data/boegen", type=str, help="Path to evaluations folder")
+    parser.add_argument("-m", "--masks_path", default="data/masks", type=str, help="Path to masks folder")
+    parser.add_argument("-v", "--vis_path", type=str, help="Debug corners detection. Path to folder")
+    opt = parser.parse_args()
+
+    # lux, luy, w, h 
+    ul_roi = (0.3, 0.1, 0.2, 0.2)
+    ur_roi = (0.75, 0.1, 0.25, 0.2)
+    # ll_roi = (0, 0.85, 0.2, 0.15)
+    ll_roi = (0.3, 0.85, 0.2, 0.15)
+    lr_roi = (0.85, 0.8, 0.15, 0.2)
+
+    rois = (ul_roi, ur_roi, ll_roi, lr_roi)
+
+    try:
+        print("Starting corners detection...")
+        start_time = time.perf_counter()
+        corners = find_corners(opt.boegen_path, opt.masks_path, rois)
+        finish_time = time.perf_counter()
+        print(f"Corners detection is finished in {finish_time - start_time} sec for {len(corners)} Boegen")
+
+        if opt.vis_path:
+            draw_corners_n_rois(corners, opt.vis_path, rois)
+    except Exception as e:
+        print(e)    
+
 
 
